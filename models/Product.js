ac93182f387e1852c76e8ed884b8691645742440
mmtakeuchi <mikemtaki@gmail.com>
2021-06-01 16:18:27 -0700

    create product model

---
+++ models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = Product = mongoose.model("product", ProductSchema);
