ef2e41672e3c1ccd3718873e700d98c4cb2c0a8d
mmtakeuchi <mikemtaki@gmail.com>
2021-06-03 14:34:29 -0700

    finished writing orders controller and routes

---
+++ controllers/ordersController.js
@@ -0,0 +1,47 @@
+const Order = require("../models/Order");
+const Cart = require("../models/Cart");
+const User = require("../models/User");
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeKEY);
+
+module.exports.getOrder = async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  console.log(order);
+  if (order) {
+    res.send(order);
+  } else {
+    res.status(400).send({ message: "No order was found." });
+  }
+};
+
+module.exports.checkout = async (req, res) => {
+  try {
+    const { source } = req.body;
+    const userId = req.params.id;
+    let user = await User.findById(userId);
+    let cart = await Cart.findById(userId);
+
+    if (cart) {
+      const charge = await stripe.charges.create({
+        amount: cart.bill,
+        currency: "usd",
+        source: source,
+        receipt_email: user.email,
+      });
+      if (!charge) throw Error("Payment failed.");
+      if (charge) {
+        const order = await Order.create({
+          userId,
+          products: cart.items,
+          bill: cart.bill,
+        });
+        const data = await Cart.findByIdAndDelete(card.id);
+        return res.status(201).send(order);
+      }
+    } else {
+      res.status(500).send({ message: "Could not retrieve cart." });
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong with checking out.");
+  }
+};
---
+++ routes/orders.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const ordersController = require("../controllers/ordersController");
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  ordersController.getOrder
+);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  ordersController.checkout
+);
+
+router.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
+
+  res.json({ id: session.id });
+});
+
+module.exports = router;
