2cc7008fdc0d59dfc3eb280c3205ff572ba86f2f
mmtakeuchi <mikemtaki@gmail.com>
2021-06-03 14:42:59 -0700

    update key in config

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:dahSrnaa35QjGYkc@cluster0.ul3iw.mongodb.net/aa-twitter?retryWrites=true&w=majority",
-  jwtSecret: "5324BB9DF7C1A71B189E6B29C7895",
-  stripeKey:
-    "sk_test_51Iy6ZlBVCIFtiE59ZjDVfUzTzevdjl0aYtD8b5A8I1EqwfpRzMCsLmDHeXPHT0qFdMzpPH2yAwEkGL7rgPvBRTMl00r3eKfate",
-};
