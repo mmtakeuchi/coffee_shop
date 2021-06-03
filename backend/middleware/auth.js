9b7a5affda4ee7d8e9de88355fedc6a243f9be29
mmtakeuchi <mikemtaki@gmail.com>
2021-06-03 14:21:33 -0700

    git problems

--- backend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    return next();
-  }
-  return res.status(401).send({ message: "Admin Token is not valid." });
-};
-
-module.exports = { isAdmin };
